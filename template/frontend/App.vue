5569b34cbc45c362e487eab285414cd21a03f944
Narutobacoshiba <haphapbk29@gmail.com>
2022-07-29 14:52:21 +0700

    update template

---
+++ template/frontend/App.vue
@@ -0,0 +1,67 @@
+<script setup>
+/*
+ * Connect2ic provides essential utilities for IC app development
+ */
+import { createClient } from "@connect2ic/core"
+import { defaultProviders } from "@connect2ic/core/providers"
+import { ConnectButton, ConnectDialog, Connect2ICProvider } from "@connect2ic/vue"
+import "@connect2ic/core/style.css"
+/*
+ * Import canister definitions like this:
+ */
+import * as nft from "../.dfx/local/canisters/nft"
+/*
+ * Some examples to get you started
+ */
+import ProcessImage from "./components/ProcessImage.vue"
+
+const client = createClient({
+  canisters: {
+    nft,
+  },
+  providers: defaultProviders,
+  globalProviderConfig: {
+    dev: import.meta.env.DEV,
+  },
+})
+</script>
+
+<template>
+  <Connect2ICProvider :client="client">
+    <div class="App">
+      <div class="auth-section">
+        <ConnectButton />
+      </div>
+      <ConnectDialog />
+      <div class="examples">
+        <ProcessImage/>
+      </div>
+    </div>
+  </Connect2ICProvider>
+</template>
+
+<style>
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+  "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+  sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #424242;
+}
+
+
+.example {
+  padding: 50px 50px;
+  min-height: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  /*border: 1px solid black;*/
+  /*background: #f4f4f4;*/
+  border-radius: 15px;
+}
+
+</style>
---
+++ template/frontend/components/ProcessImage.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="process-image" >
+    <template v-if="wallet">
+        <div class="button-section">
+            <div class="button-t"> Load Image </div>
+            <div class="button-t"> Mint </div>
+        </div>
+        <div class="images-section">
+
+        </div>
+    </template>
+    <template v-else>
+        <p> Connect with a wallet to access this function </p>
+    </template>
+  </div>
+</template>
+
+<script setup>
+import { useCanister, useWallet } from "@connect2ic/vue"
+
+const [nft] = useCanister("nft")
+
+const [wallet] = useWallet()
+
+</script>
+
+<style>
+.process-image{
+    height: 600px;
+    width: 700px;
+}
+
+.button-section{
+    display: flex;
+    justify-content: space-between;
+}
+
+.button-t{
+    height: 100px;
+    width: 50px;
+    border: 1px solid black;
+}
+
+.images-section{
+    width: 700px;
+    height: 400px;
+    border: 1px solid black;
+}
+
+</style>
