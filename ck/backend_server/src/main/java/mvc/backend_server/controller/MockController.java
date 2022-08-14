217d333cffb1c53a2218905aa125f2d69041d393
Tran Thi Nguyet Ha <hattnhe153222@fpt.edu.vn>
2022-08-14 23:31:18 +0700

    Test mock controller

--- ck/backend_server/src/main/java/mvc/backend_server/controller/MockController.java
+++ ck/backend_server/src/main/java/mvc/backend_server/controller/MockController.java
@@ -1,13 +1,13 @@
 package mvc.backend_server.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/mock")
 public class MockController {
+
     @GetMapping
     public String testGetMethod(){
         return null;
