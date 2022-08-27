7f7b7e21877824436fb046bc62ba2315ef79a798
GiangTT1909 <giangtong199@gmail.com>
2022-08-28 03:11:09 +0700

    fix axios

--- ck/frontend_template/frontend/src/services/plan.services.js
+++ ck/frontend_template/frontend/src/services/plan.services.js
@@ -1,22 +1,38 @@
 import { BaseService, ErrorWrapper, ResponseWrapper } from './base.js'
 
-export class PlanServices extends BaseService{
-    static get entity () {
+export class PlanServices extends BaseService {
+    static get entity() {
         return 'trip'
     }
 
     static async createPlan(param) {
-      try {
-        const response = await axios.get("/bapi/trip/generate", {
-          params: param,
-        })
-        // const responses = await axios.get("/dapi/trip/generate", {
-        //   params: param,
-        // })
-        //const response = await this.request({ auth: false }).post(`${this.entity}/generate`,{"param":param})
-        return new ResponseWrapper(response, response.data)
-      } catch (error) {
-        throw new ErrorWrapper(error)
-      }
+        try {
+            const response = await axios.get("/bapi/trip/generate", {
+                    params: param,
+                })
+                // const responses = await axios.get("/dapi/trip/generate", {
+                //   params: param,
+                // })
+                //const response = await this.request({ auth: false }).post(`${this.entity}/generate`,{"param":param})
+            return new ResponseWrapper(response, response.data)
+        } catch (error) {
+            throw new ErrorWrapper(error)
+        }
+    }
+
+    static async getTrips(param) {
+        try {
+            const response = await axios.get("/bapi/trip/getByAccount/" + param, {
+                    params: param,
+                })
+                // const responses = await axios.get("/dapi/trip/generate", {
+                //   params: param,
+                // })
+                //const response = await this.request({ auth: false }).post(`${this.entity}/generate`,{"param":param})
+            return new ResponseWrapper(response, response.data)
+        } catch (error) {
+            throw new ErrorWrapper(error)
+        }
+
     }
 }
